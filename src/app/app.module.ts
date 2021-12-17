1dec4d427e1f05a69f3b5d7419ef13b86de2cedc
leilamyar <leilamyar.dev@gmail.com>
2021-12-17 14:10:14 +0100

    add(app.module) : httpClient and forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FilmsComponent } from './components/films/films.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilmsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, // see tuto from Fireship : https://www.youtube.com/watch?v=JeeUY6WaXiA
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
