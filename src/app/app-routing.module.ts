5874cc778883c8ea6e19e2325cf67e7d4ff4047e
leilamyar <leilamyar.dev@gmail.com>
2021-12-17 18:16:36 +0100

    feat(authGuard) : cannot see films if not loggedIn

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FilmsComponent } from './components/films/films.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { AuthGuard } from './guards/auth.guard';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'films', component: FilmsComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -10,8 +11,9 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
 
   loginForm: any = FormGroup;
+  private users: any[] = [];
 
-  constructor(private fb: FormBuilder, private router: Router) { }
+  constructor(private fb: FormBuilder, private router: Router, private authSv: AuthService) { }
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
@@ -21,12 +23,27 @@ export class LoginComponent implements OnInit {
     });
     // Extract values from the form:
     // this.loginForm.valueChanges.subscribe(console.log);
-
+    this.authSv.getUser()
+      .subscribe((data) => {
+        this.users = data;
+      });
   };
 
-  submitLogin(data: any) {
-    // console.log('data from Form::', data);
+  submitLogin(inputData: any) {
+    // console.log('inputData from Form::', inputData);
+    if (inputData.username) {
+      let fromDb = this.users.find(u => u.username === inputData.username);
+      if (fromDb.password === inputData.password) {
+        // console.log('User is valid');
+        localStorage.setItem('isLoggedIn', 'true');
+        this.router.navigate(['films']);
+      } else {
+        console.log('User is not valid');
+      }
+    } else {
+      console.log('Please enter valid username & password');
 
+    }
   };
 
   navigateToSignUp() {
