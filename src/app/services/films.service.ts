71d6ed1dd4ac95d5ff9ec79d9bf2aa86e57e69db
leilamyar <leilamyar.dev@gmail.com>
2021-12-17 16:14:52 +0100

    remove(import) : filmSv unused of import

--- src/app/services/films.service.ts
+++ src/app/services/films.service.ts
@@ -1,20 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FilmsService {
 
+  // TODO: put root url into .env files
   private apiUrl = 'http://localhost:5000/films/';
 
   constructor(private http: HttpClient) { }
 
   getFilms(): Observable<any[]> {
     const res = this.http.get<any[]>(this.apiUrl);
-    console.log('res:::', res);
-
     return res;
   }
 }
