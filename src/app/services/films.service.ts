4983d141ad084737cc06e7bf678867467dc18f9e
leilamyar <leilamyar.dev@gmail.com>
2021-12-17 17:56:11 +0100

    refactor(filmService) : use baseUrl from env file

--- src/app/services/films.service.ts
+++ src/app/services/films.service.ts
@@ -1,19 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment as env } from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FilmsService {
 
-  // TODO: put root url into .env files
-  private apiUrl = 'http://localhost:5000/films/';
+  private baseUrl = env.baseUrl;
 
   constructor(private http: HttpClient) { }
 
   getFilms(): Observable<any[]> {
-    const res = this.http.get<any[]>(this.apiUrl);
+    const res = this.http.get<any[]>(this.baseUrl + '/films');
     return res;
   }
 }
