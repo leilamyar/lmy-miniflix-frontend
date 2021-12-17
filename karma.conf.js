d4cf141f9232ffbe443a6e2ccc232a5381c103c4
leilamyar <leilamyar.dev@gmail.com>
2021-12-17 20:11:22 +0100

    refactor(app) : rename project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ng-forms-playground'),
+      dir: require('path').join(__dirname, './coverage/lmy-miniflix-frontend'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng-forms-playground'`, () => {
+  it(`should have as title 'lmy-miniflix-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-forms-playground');
+    expect(app.title).toEqual('lmy-miniflix-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ng-forms-playground app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('lmy-miniflix-frontend app is running!');
   });
 });
