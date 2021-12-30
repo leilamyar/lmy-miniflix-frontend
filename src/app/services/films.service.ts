98f7185770d9a3cdc53b752e528e2256497f8717
leilamyar <leilamyar.dev@gmail.com>
2021-12-30 17:35:01 +0100

    feat(filmSv) : get film by id

--- src/app/services/films.service.ts
+++ src/app/services/films.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment as env } from "../../environments/environment";
 
@@ -15,5 +15,18 @@ export class FilmsService {
   getFilms(): Observable<any[]> {
     const res = this.http.get<any[]>(this.baseUrl + '/films');
     return res;
+  }
+
+  // getFilmsByIdsList(ids: number[]): Observable<any[]> {
+  //   const params = new HttpParams().set('ids', ids);
+  //   return this.http
+  //     .get<any>(this.baseUrl + '/films/', { params })
+  //   // return req;
+  // }
+  getFilmById(id: number): Observable<any> {
+    const params = new HttpParams().set('id', id);
+    return this.http
+      .get<any>(this.baseUrl + '/films/', { params })
+    // return req;
   }
 }
