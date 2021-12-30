76edadc36d671c74e3ab74c30f95d310553c4d8c
leilamyar <leilamyar.dev@gmail.com>
2021-12-30 17:40:06 +0100

    add(filmIds) : set film ids in LS - to be removed

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -37,6 +37,10 @@ export class LoginComponent implements OnInit {
       if (this.user.password === this.fromForm.password) {
         localStorage.setItem('isLoggedIn', 'true');
         localStorage.setItem('user', this.user.username);
+
+        localStorage.setItem('list', this.user.herList.join(','));
+        // TODO: replace ls list (ie, user) by Store
+
         this.router.navigate(['browse']);
       } else {
         this.msg = 'The password is not correct';
