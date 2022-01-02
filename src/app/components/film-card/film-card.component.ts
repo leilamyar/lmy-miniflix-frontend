6e62ef86f4443773b18bfa8258d5b00ceb4b3301
leilamyar <leilamyar.dev@gmail.com>
2022-01-02 17:43:59 +0100

    fix(myList) : films added twice

--- src/app/components/film-card/film-card.component.ts
+++ src/app/components/film-card/film-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { AppState } from 'src/app/models/AppState';
 import { UserService } from 'src/app/services/user.service';
 import { appStateSelector, myListSelector } from 'src/app/utils/appState.utils';
@@ -21,16 +21,20 @@ export class FilmCardComponent implements OnInit, OnDestroy {
     if (appState) {
       const appState = appStateSelector(localStorage);
       const currentMyList = myListSelector(appState);
-      const newAppState: AppState = {
-        ...appState,
-        myList: [...currentMyList, filmIdToAdd],
-      };
 
-      this.userSv.updateUserList(newAppState)
-        .subscribe(data => {
-          localStorage.removeItem(appState);
-          localStorage.setItem('appState', JSON.stringify(newAppState));
-        });
+      console.log('CONTAINS ? :', currentMyList.includes(filmIdToAdd));
+      if (!currentMyList.includes(filmIdToAdd)) {
+
+        const newAppState: AppState = {
+          ...appState,
+          myList: [...currentMyList, filmIdToAdd],
+        };
+        this.userSv.updateUserList(newAppState)
+          .subscribe(data => {
+            localStorage.removeItem(appState);
+            localStorage.setItem('appState', JSON.stringify(newAppState));
+          });
+      }
     } else {
       console.error('Error with AppState Selector');
       throw new Error("Error with AppState Selector using LocalStorage");
