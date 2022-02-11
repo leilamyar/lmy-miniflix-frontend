b3f4eb3f2d640372f8b2d067ca78452db6567166
leilamyar <leilamyar.dev@gmail.com>
2022-02-11 16:41:30 +0100

    cleanup(MyList)

--- src/app/pages/my-list/my-list.component.ts
+++ src/app/pages/my-list/my-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { forkJoin, map } from 'rxjs';
+import { forkJoin, map, Subscription } from 'rxjs';
 import { Film } from 'src/app/models/Film';
 
 import { FilmsService } from 'src/app/services/films.service';
@@ -12,31 +12,21 @@ import { FilmsService } from 'src/app/services/films.service';
 export class MyListComponent implements OnInit, OnDestroy {
 
   filmList: any[];
+  private _subscription: Subscription;
 
   constructor(private filmsSv: FilmsService) { }
 
   ngOnInit(): void {
-
-    const result = this.filmsSv.getFilms().pipe(
+    this.filmsSv.getFilms().pipe(
       map((films) => {
         const httpCall$ = films.map((f: Film) => this.filmsSv.getFilmById(f.id));
-        console.log('httpCalls:::', httpCall$);
-        let sss = forkJoin(httpCall$)
+        this._subscription = forkJoin(httpCall$)
           .subscribe((data: any) => this.filmList = data.map((curr: any) => curr[0]));
-        console.log('sss to unsubs ?', sss);
-
       }),
     );
-    // const httpCall$ = filmIds.map((id: number) => this.filmsSv.getFilmById(id));
-    // forkJoin(httpCall$)
-    //   .subscribe((data: any) => this.filmList = data.map((curr: any) => curr[0]));
-
-    console.log('RESULT::', result);
-
   }
 
   ngOnDestroy(): void {
-    console.log('[MyListComp] Destroyed');
-
+    this._subscription.unsubscribe();
   }
 }
